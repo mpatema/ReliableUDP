85ca6576704e647c781dbfc872128ab7cb43528a
mpatema <mpateman9484@conestogac.on.ca>
2022-02-10 22:58:12 -0500

    file operations started

--- FileOperations.cpp
+++ FileOperations.cpp
@@ -1,4 +1,25 @@
+#include <string>
+
+using namespace std;
 ///
 /// This file will handle file operations such as checking for compatibility of files, 
 /// file I/O, saving, etc.
 /// 
+/// 
+
+
+
+int OpenFile (const char* fileName[]) {
+	FILE* newFile = fopen(*fileName, "r");
+	if (newFile != NULL) {
+		
+		return 0;
+	}
+	else {
+		//return error code 
+		return 1;
+	}
+}
+
+int FileExtensionVal () {
+}
